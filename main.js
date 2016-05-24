e3092d13ea40d0371acdd6f013173810276b1eeb
Tomislav Fabeta <tomislav.fabeta@degordian.com>
2016-05-24 10:49:36 +0200

    global module created

--- main.js
+++ main.js
@@ -1,68 +1,63 @@
-/**
- * Created by tomislavfabeta on 10/05/16.
- */
+#!/usr/bin/env node
 
 require("./global/all");
+var CLASS = global.CLASS;
+var docmaster = new CLASS.docmaster();
 
-module.exports = function(){
-    var CLASS = global.CLASS;
-    var docmaster = new CLASS.docmaster();
+if (process.argv.length <= 2){
+    console.log("Please set input and output file path!");
+    process.exit(-1);
+}
 
-    if (process.argv.length <= 2){
-        console.log("Please set input and output file path!");
-        process.exit(-1);
-    }
-
-    var defaultParams = {
-        "-o": {description:"Output file", varName:"output"},
-        "-i": {description:"Input file", varName:"input"},
-        "-e": {description:"Postman enviroment", varName:"enviroment"},
-        "--iformat": {description:"Input format", varName:"inputFormat"}
-    };
-    var params = {
-        "enviroment": null,
-        "inputFormat": "postman"
-    };
-
-    for (var i = 2; i<process.argv.length; i++){
-        var param = process.argv[i];
-
-        if (defaultParams.hasOwnProperty(param)){
-            i = i+1;
-
-            if (process.argv.length <= i){
-                console.log("Missing parameter ("+param+")");
-                process.exit(-1);
-            }
-
-            param = defaultParams[param].varName;
-
-            params[param] = process.argv[i];
-        }
-
-    }
-
-    if (!params["input"] || !params["output"]){
-        console.log("Missing parameter!");
-        process.exit(-1);
-    }
-
-    docmaster.setEnviroment(params.enviroment);
-    docmaster.setInputPath(params.input);
-    docmaster.setOutputPath(params.output);
-    docmaster.setFormat(params.inputFormat);
-    docmaster.export(done);
-
-    function done(err, res){
-        if (err){
-            console.log("Error! - ", err);
-        }else{
-            console.log("");
-            console.log("Finished!");
-            console.log("__________________");
-            console.log(res);
-            console.log("__________________");
-            console.log("");
-        }
-    }
+var defaultParams = {
+    "-o": {description:"Output file", varName:"output"},
+    "-i": {description:"Input file", varName:"input"},
+    "-e": {description:"Postman enviroment", varName:"enviroment"},
+    "--iformat": {description:"Input format", varName:"inputFormat"}
 };
+var params = {
+    "enviroment": null,
+    "inputFormat": "postman"
+};
+
+for (var i = 2; i<process.argv.length; i++){
+    var param = process.argv[i];
+
+    if (defaultParams.hasOwnProperty(param)){
+        i = i+1;
+
+        if (process.argv.length <= i){
+            console.log("Missing parameter ("+param+")");
+            process.exit(-1);
+        }
+
+        param = defaultParams[param].varName;
+
+        params[param] = process.argv[i];
+    }
+
+}
+
+if (!params["input"] || !params["output"]){
+    console.log("Missing parameter!");
+    process.exit(-1);
+}
+
+docmaster.setEnviroment(params.enviroment);
+docmaster.setInputPath(params.input);
+docmaster.setOutputPath(params.output);
+docmaster.setFormat(params.inputFormat);
+docmaster.export(done);
+
+function done(err, res){
+    if (err){
+        console.log("Error! - ", err);
+    }else{
+        console.log("");
+        console.log("Finished!");
+        console.log("__________________");
+        console.log(res);
+        console.log("__________________");
+        console.log("");
+    }
+}
